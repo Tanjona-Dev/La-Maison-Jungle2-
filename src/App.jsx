a8dd107bb40b9e5f670d940d10c6a3e38f507004
Tanjona-Dev <rabemangasoatanjonanasa@gmail.com>
2025-03-16 12:11:36 +0300

    Categorie et Panier mise a jours et marche bien

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import Banner from "./Components/Banner";
 import Shopping from "./Components/Shopping";
-import Panier from './Components/Cart'
-
+import Panier from "./Components/Cart";
+import "./Style/App.css"
 function App() {
   const [cart, setCart] = useState([]);
   return (
-    <>
+    <div >
       <Banner />
-      < Panier cart={cart} setCart={setCart} />
+      <div className="flexBody">
+      <Panier cart={cart} setCart={setCart} />
       <Shopping cart={cart} setCart={setCart} />
-    </>
+      </div>
+
+    </div>
   );
 }
 
--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,6 +1,12 @@
+import '../Style/Panier.css'
+import { useState } from 'react';
+
 function Panier({cart, setCart}) {
-  return (
-    <div>
+    const [isOpen, setIsOpen] = useState(true)
+    const Total = cart.reduce((acc, plant)=> acc + plant.amount * plant.price,0)
+  return isOpen ? (
+    <div className='panier'>
+        <button onClick={()=> setIsOpen(false)}>fermer</button>
       <h3>Panier</h3>
       <ul>
         {cart.map(({name, price, amount},index)=> <li key={index}>
@@ -11,7 +17,12 @@ function Panier({cart, setCart}) {
             Prix : {price} euro
         </li>)}
       </ul>
+      <span>Total : {Total} euro</span>
+      <button onClick={()=>setCart([])}>vider</button>
     </div>
-  );
+
+  ) : (
+    <button onClick={()=>setIsOpen(true)}>Ouvrir le Panier</button>
+  )
 }
 export default Panier;
